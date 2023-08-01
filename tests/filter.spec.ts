7139aa722264cd05933acf5eb4179af15911468e
Thomas Kirkwood <Tkirkwood@spartaglobal.com>
2023-08-01 09:45:52 +0100

    pretty-ified scripts, added tags to not run contact us on prod

--- tests/filter.spec.ts
+++ tests/filter.spec.ts
@@ -1,25 +1,28 @@
 import { test, expect } from "@playwright/test";
 
-
-
-
 test("filter app simple search", async ({ page }) => {
-    // can be any page we start on
-    await page.goto(process.env.URL+"results/search-results?query=ISRCTN33951209&location=Strabane");
-    page.locator('#Recruiting').uncheck();
-    // check the logo is appearing
-    await expect(page.locator('article')).toHaveCount(1);
+  // can be any page we start on
+  await page.goto(
+    process.env.URL +
+      "results/search-results?query=ISRCTN33951209&location=Strabane"
+  );
+  page.locator("#Recruiting").uncheck();
+  // check the logo is appearing
+  await expect(page.locator("article")).toHaveCount(1);
 });
 
 test("distance check", async ({ page }) => {
-    // can be any page we start on
-    await page.goto(process.env.URL+"results/search-results?query=ISRCTN33951209&location=londonderry");
-    page.locator('#Recruiting').uncheck();
-    // check the logo is appearing
-    await expect(page.locator('article')).toHaveCount(1);
+  // can be any page we start on
+  await page.goto(
+    process.env.URL +
+      "results/search-results?query=ISRCTN33951209&location=londonderry"
+  );
+  page.locator("#Recruiting").uncheck();
+  // check the logo is appearing
+  await expect(page.locator("article")).toHaveCount(1);
 
-    page.locator('#FilterItemRadioButton10').click();
-    await expect(page.locator('article')).toHaveCount(0);
-    page.locator('#FilterItemRadioButton1000').click();
-    await expect(page.locator('article')).toHaveCount(1);
+  page.locator("#FilterItemRadioButton10").click();
+  await expect(page.locator("article")).toHaveCount(0);
+  page.locator("#FilterItemRadioButton1000").click();
+  await expect(page.locator("article")).toHaveCount(1);
 });
--- tests/trialsDetails.spec.ts
+++ tests/trialsDetails.spec.ts
@@ -2,57 +2,81 @@ import { test, expect } from "@playwright/test";
 
 // the id of the study ISRCTN13072268 on prod and uat
 let trialId = 9570;
-if(process.env.URL != null && process.env.URL.includes("uat")){
+if (process.env.URL != null && process.env.URL.includes("uat")) {
   trialId = 9858;
 }
 
-
 test("advert appears on page", async ({ page }) => {
-    // using a dementia study for the JDR adverts
-    await page.goto(process.env.URL+"trial-details/trial-detail?trialId="+trialId+"&location=&distance=");
-  
-    const advert = page.locator(".bpor-advert").first();
-  
-    // Expect there to be an advert on the page
-    await expect(advert).toBeTruthy();
-  });
+  // using a dementia study for the JDR adverts
+  await page.goto(
+    process.env.URL +
+      "trial-details/trial-detail?trialId=" +
+      trialId +
+      "&location=&distance="
+  );
 
-  test("JDR advert points to correct URL", async ({ page }) => {
-    // using a dementia study for the JDR adverts
-    await page.goto(process.env.URL+"trial-details/trial-detail?trialId="+trialId+"&location=&distance=");
-  
-    const advertLink = page.locator("#bpor-advert-condition").first();
-    
-  
-    // Expect the JDR advert to point to JDR
-    await expect(advertLink).toHaveAttribute('href','https://www.joindementiaresearch.nihr.ac.uk');
-  });
+  const advert = page.locator(".bpor-advert").first();
 
-  test("check study sites button", async ({ page }) => {
+  // Expect there to be an advert on the page
+  await expect(advert).toBeTruthy();
+});
 
-    // using a dementia study for the JDR adverts
-    await page.goto(process.env.URL+"trial-details/trial-detail?trialId="+trialId+"&location=&distance=");
-  
-    // check the study sites button shows the location list and that it contains the correct locations
-    const locationList = page.locator(".location-list");
-    await expect(locationList).toHaveClass(/d-none/);
-    page.locator(".location-toggle").click();
-    await expect(locationList).not.toHaveClass(/d-none/);
-    const studySite = locationList.getByText(/Wickford/);
-    expect(studySite).toBeTruthy();
-  });
+// commenting out to focus tests on hard-coded values and data provided from the backend
 
-  test("summary collapsable link", async ({ page }) => {
-    // using a dementia study for the JDR adverts
-    await page.goto(process.env.URL+"trial-details/trial-detail?trialId="+trialId+"&location=&distance=");
-  
-    // check the collapable links
-    const collapsableLink = page.locator(".collapsable__action").first();
-    const collapsableTitle = page.locator('.collapsable__title').first();
-    const collapsableContent = page.locator('.collapsable__content').first();
-    await expect(collapsableTitle).toHaveText(/Study summary/);
-    await expect(collapsableContent).toHaveClass(/d-none/);
-    collapsableLink.click();
-    await expect(collapsableContent).not.toHaveClass(/d-none/);
-    await expect(collapsableContent).toHaveText(/This information is provided directly by researchers and we recognise that it isn't always easy to understand./);
-  });
+// test("JDR advert points to correct URL", async ({ page }) => {
+//   // using a dementia study for the JDR adverts
+//   await page.goto(
+//     process.env.URL +
+//       "trial-details/trial-detail?trialId=" +
+//       trialId +
+//       "&location=&distance="
+//   );
+//   const advertLink = page.locator("#bpor-advert-condition")[1];
+//   console.log(advertLink);
+
+//   // Expect the JDR advert to point to JDR
+//   await expect(advertLink).toHaveAttribute(
+//     "href",
+//     "https://www.joindementiaresearch.nihr.ac.uk"
+//   );
+// });
+
+test("check study sites button", async ({ page }) => {
+  // using a dementia study for the JDR adverts
+  await page.goto(
+    process.env.URL +
+      "trial-details/trial-detail?trialId=" +
+      trialId +
+      "&location=&distance="
+  );
+
+  // check the study sites button shows the location list and that it contains the correct locations
+  const locationList = page.locator(".location-list");
+  await expect(locationList).toHaveClass(/d-none/);
+  page.locator(".location-toggle").click();
+  await expect(locationList).not.toHaveClass(/d-none/);
+  const studySite = locationList.getByText(/Wickford/);
+  expect(studySite).toBeTruthy();
+});
+
+test("summary collapsable link", async ({ page }) => {
+  // using a dementia study for the JDR adverts
+  await page.goto(
+    process.env.URL +
+      "trial-details/trial-detail?trialId=" +
+      trialId +
+      "&location=&distance="
+  );
+
+  // check the collapable links
+  const collapsableLink = page.locator(".collapsable__action").first();
+  const collapsableTitle = page.locator(".collapsable__title").first();
+  const collapsableContent = page.locator(".collapsable__content").first();
+  await expect(collapsableTitle).toHaveText(/Study summary/);
+  await expect(collapsableContent).toHaveClass(/d-none/);
+  collapsableLink.click();
+  await expect(collapsableContent).not.toHaveClass(/d-none/);
+  await expect(collapsableContent).toHaveText(
+    /This information is provided directly by researchers and we recognise that it isn't always easy to understand./
+  );
+});
